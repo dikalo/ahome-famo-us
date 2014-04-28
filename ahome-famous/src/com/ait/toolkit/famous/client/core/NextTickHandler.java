659042b30b125828f41d4a04c3aeff37f90c7cd6
eemi2010 <jazzmatadazz@gmail.com>
2014-04-29 00:21:53 +0200

    Implemented NextTickHandler

---
+++ ahome-famous/src/com/ait/toolkit/famous/client/core/NextTickHandler.java
@@ -0,0 +1,7 @@
+package com.ait.toolkit.famous.client.core;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public interface NextTickHandler {
+	public void onNextTick(JavaScriptObject window);
+}
